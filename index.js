07535e4a153e728cfa7154680d2c252979190ba9
doleaga2 <oleaga.diana@gmail.com>
2022-08-31 19:27:00 -0400

    assessment

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 // Obtain the img element, and assign it to the image variable
 let image
-
+document.getElementById('image')
 // Obtain the 'shrink-grow' button, and assign it to the changeSize variable
 let changeSize
+document.querySelector('button').onclick = function() {
+  var image = document.getElementById('shrink-grow');
+  image.style.width ='156px';
+  image.style.height = 'auto';
+}
 
 //DO NOT CHANGE THE FOLLOWING CODE
 changeSize.addEventListener("click", function() {
